1bbac9f4cc7e37fa8329490debd56352f61c056c
Axel Brobeck <axel.brobeck@bizone.se>
2024-07-11 10:42:28 +0200

    day one finished

---
+++ src/main.rs
@@ -0,0 +1,69 @@
+use std::fs;
+
+fn main() {
+
+}   
+
+fn _day_1_1() {
+    let input = fs::read_to_string("input.txt").unwrap();
+
+    let ans: u32 = input.lines().map(|line| {
+        let first_digit = line.chars().find_map(|c| c.to_digit(10)).unwrap();
+        let last_digit = line.chars().rev().find_map(|c| c.to_digit(10)).unwrap();
+        first_digit * 10 + last_digit
+    }).sum();
+
+    println!("{ans}")
+}
+
+fn _day_1_2() {
+    let words = [
+        ("one", 1),
+        ("two", 2), 
+        ("three", 3), 
+        ("four", 4), 
+        ("five", 5), 
+        ("six", 6), 
+        ("seven", 7), 
+        ("eight", 8), 
+        ("nine", 9),
+        ("1", 1),
+        ("2", 2), 
+        ("3", 3), 
+        ("4", 4), 
+        ("5", 5), 
+        ("6", 6), 
+        ("7", 7), 
+        ("8", 8), 
+        ("9", 9),
+    ];
+
+    let input = fs::read_to_string("input.txt").unwrap();
+
+    let ans = input.lines().map(|line| {
+        let mut min_first_index = None;
+        let mut first_value = 0;
+        let mut max_last_index = None;
+        let mut last_value = 0;
+
+        for (word, value) in words {
+            if let Some(index) = line.find(word) {
+                if min_first_index.map_or(true, |x| index < x) {
+                    min_first_index = Some(index);
+                    first_value = value;
+                }
+            }
+
+            if let Some(index) = line.rfind(word) {
+                if max_last_index.map_or(true, |x| x < index) {
+                    max_last_index = Some(index);
+                    last_value = value;
+                }
+            }
+        }
+
+        first_value * 10 + last_value
+    }).sum::<i32>();
+
+    println!("{ans}")
+}
