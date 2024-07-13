be28515738a8deb9ccb62d7f3e6542220c7dbbba
brobeck <axel.brobeck@gmail.com>
2024-07-13 18:03:04 +0200

    day 4 part 2

--- src/day04.rs
+++ src/day04.rs
@@ -1,3 +1,4 @@
+use std::cmp;
 use std::fs;
 
 pub fn run(task: &str) {
@@ -52,7 +53,41 @@ fn solve_task_1() {
 }
 
 fn solve_task_2() {
-    let _input = fs::read_to_string("inputs/input_4.txt").unwrap();
-    eprintln!("This task is not implemented yet!");
-    std::process::exit(1);
+    let input = fs::read_to_string("input/input_4.txt").unwrap();
+    let mut card_number_array: [usize; 100] = [0; 100];
+    let mut number_of_that_card: [usize; 189] = [1; 189];
+    let mut set_number_array: [usize; 10] = [0; 10];
+
+    for (enumeration, line) in input.lines().enumerate() {
+        let (card_numbers, guess_numbers) = line.split_once('|').unwrap();
+
+        for (index_set_array, card_number) in card_numbers
+            .split_whitespace()
+            .skip(1)
+            .filter_map(|s| s.parse::<usize>().ok())
+            .enumerate()
+        {
+            card_number_array[card_number] = 1;
+            set_number_array[index_set_array] = card_number;
+        }
+
+        let correct_guesses = guess_numbers
+            .split_whitespace()
+            .filter_map(|s| s.parse::<usize>().ok())
+            .map(|guess_number| card_number_array[guess_number])
+            .sum::<usize>();
+
+        for card_number in set_number_array {
+            card_number_array[card_number] = 0;
+        }
+
+        let to_index: usize = cmp::min(189, enumeration + 1 + correct_guesses);
+        for i in enumeration + 1..to_index {
+            number_of_that_card[i] += number_of_that_card[enumeration];
+        }
+    }
+
+    let ans: usize = number_of_that_card.iter().sum();
+
+    println!("{ans}")
 }
